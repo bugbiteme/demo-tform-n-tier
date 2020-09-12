d2773797ad7675d5a14d6f7a04777afe0d5e1ed3
bugbiteme <leon.s.levy@gmail.com>
2020-09-12 09:34:08 -0700

    add autoscaling

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+module "autoscaling" {
+  source      = "./modules/autoscaling"
+  namespace   = var.namespace
+  ssh_keypair = var.ssh_keypair
+ 
+  vpc       = module.networking.vpc
+  sg        = module.networking.sg
+  db_config = module.database.db_config
+}
+ 
 module "database" {
   source    = "./modules/database"
   namespace = var.namespace
---
+++ modules/autoscaling/main.tf
@@ -0,0 +1,60 @@
+module "iam_instance_profile" {
+  source  = "llevy/iip/aws"
+  actions = ["logs:*", "rds:*"]
+}
+ 
+data "template_cloudinit_config" "config" {
+  gzip          = true
+  base64_encode = true
+  part {
+    content_type = "text/cloud-config"
+    content      = templatefile("${path.module}/cloud_config.yaml", var.db_config)
+  }
+}
+ 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+  }
+  owners = ["099720109477"]
+}
+ 
+resource "aws_launch_template" "webserver" {
+  name_prefix   = var.namespace
+  image_id      = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  user_data     = data.template_cloudinit_config.config.rendered
+  key_name            = var.ssh_keypair
+  iam_instance_profile {
+    name = module.iam_instance_profile.name
+  }
+  vpc_security_group_ids = [var.sg.websvr]
+}
+ 
+resource "aws_autoscaling_group" "webserver" {
+  name                = "${var.namespace}-asg"
+  min_size            = 1
+  max_size            = 3
+  vpc_zone_identifier = var.vpc.private_subnets
+  target_group_arns   = module.alb.target_group_arns
+  launch_template {
+    id = aws_launch_template.webserver.id
+    version = aws_launch_template.webserver.latest_version
+  }
+}
+ 
+module "alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 4.0"
+  load_balancer_name       = "${var.namespace}-alb"
+  security_groups          = [var.sg.lb]
+  subnets                  = var.vpc.public_subnets
+  vpc_id                   = var.vpc.vpc_id
+  logging_enabled          = false
+  http_tcp_listeners       = [{ port = 80, protocol = "HTTP" }]
+  http_tcp_listeners_count = "1"
+  target_groups            = [{ name = "websvr", backend_protocol = "HTTP", backend_port = 8080 }]
+  target_groups_count      = "1"
+}
