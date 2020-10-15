cc0c22bb0f0fa12cea8348f7a4bf1c1bc228da68
bugbiteme <leon.s.levy@gmail.com>
2020-10-15 10:26:03 -0700

    terraform fmt

--- main.tf
+++ main.tf
@@ -2,20 +2,20 @@ module "autoscaling" {
   source      = "./modules/autoscaling"
   namespace   = var.namespace
   ssh_keypair = var.ssh_keypair
- 
+
   vpc       = module.networking.vpc
   sg        = module.networking.sg
   db_config = module.database.db_config
 }
- 
+
 module "database" {
   source    = "./modules/database"
   namespace = var.namespace
- 
+
   vpc = module.networking.vpc
   sg  = module.networking.sg
 }
- 
+
 module "networking" {
   source    = "./modules/networking"
   namespace = var.namespace
